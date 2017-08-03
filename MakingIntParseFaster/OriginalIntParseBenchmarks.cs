0cc282eb4b882511b6c888e7267d9478fa64b685
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2017-08-03 17:02:08 +0300

    Missing?

---
+++ MakingIntParseFaster/OriginalIntParseBenchmarks.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace MakingIntParseFaster
+{
+    [Config(typeof(Config))]
+    public class OriginalIntParseBenchmarks
+    {
+        [Benchmark]
+        public int OneDigit()
+        {
+            return int.Parse("1");
+        }
+
+        [Benchmark]
+        public int MaxValue()
+        {
+            return int.Parse("2147483647");
+        }
+
+        [Benchmark]
+        public int Sign()
+        {
+            return int.Parse("-2147483648");
+        }
+
+        [Benchmark]
+        public int Whitespaces()
+        {
+            return int.Parse("          -2147483648          ");
+        }
+
+        [Benchmark]
+        public int FasterOneDigit()
+        {
+            return OriginInt.Parse("1");
+        }
+
+        [Benchmark]
+        public int FasterMaxValue()
+        {
+            return OriginInt.Parse("2147483647");
+        }
+
+        [Benchmark]
+        public int FasterSign()
+        {
+            return OriginInt.Parse("-2147483648");
+        }
+
+        [Benchmark]
+        public int FasterWhitespaces()
+        {
+            return OriginInt.Parse("          -2147483648          ");
+        }
+    }
+}
