569cf520818e86cad376d125f0f29d2ee3d088fe
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2017-08-03 17:01:52 +0300

    V2 copy paste

---
+++ MakingIntParseFaster/IntParseFasterBenchmarks.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace MakingIntParseFaster
+{
+    [Config(typeof(Config))]
+    public class IntParseFasterBenchmarks
+    {
+        [Benchmark]
+        public int OneDigit()
+        {
+            return FasterInt.Parse("1");
+        }
+
+        [Benchmark]
+        public int MaxValue()
+        {
+            return FasterInt.Parse("2147483647");
+        }
+
+        [Benchmark]
+        public int Sign()
+        {
+            return FasterInt.Parse("-2147483648");
+        }
+
+        [Benchmark]
+        public int Whitespaces()
+        {
+            return FasterInt.Parse("          -2147483648          ");
+        }
+
+        [Benchmark]
+        public int OneDigitV2()
+        {
+            return V2.FasterInt.Parse("1");
+        }
+
+        [Benchmark]
+        public int MaxValueV2()
+        {
+            return V2.FasterInt.Parse("2147483647");
+        }
+
+        [Benchmark]
+        public int SignV2()
+        {
+            return V2.FasterInt.Parse("-2147483648");
+        }
+
+        [Benchmark]
+        public int WhitespacesV2()
+        {
+            return V2.FasterInt.Parse("          -2147483648          ");
+        }
+    }
+}
--- MakingIntParseFaster/Program.cs
+++ MakingIntParseFaster/Program.cs
@@ -8,7 +8,7 @@ namespace MakingIntParseFaster
     {
         static void Main(string[] args)
         {
-            BenchmarkRunner.Run<OriginalIntParseBenchmarks>();
+            BenchmarkRunner.Run<IntParseFasterBenchmarks>();
         }
     }
 }
