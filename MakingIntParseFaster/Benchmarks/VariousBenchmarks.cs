0031eb4b2652262d20d9956c0fb1d13b9c729ea7
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2017-08-07 16:09:58 +0300

    var bench

---
+++ MakingIntParseFaster/Benchmarks/VariousBenchmarks.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace MakingIntParseFaster.Benchmarks
+{
+    public class VariousBenchmarks
+    {
+        [Benchmark]
+        public unsafe int Fixes()
+        {
+            var sum = 0;
+            fixed (char* sptr = "1234567890")
+            {
+                var cptr = sptr;
+                while((uint)(*cptr - '0') <= 9)
+                {
+                    sum += (*cptr - '0');
+                    cptr++;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
